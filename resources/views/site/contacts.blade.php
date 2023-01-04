4397e5d2c68425463e9a23f8d5ee8de347520b02
brenomol <emaildobrenomol@gmail.com>
2023-01-04 14:26:46 -0300

    Alteracao qr-code

--- resources/views/site/contacts.blade.php
+++ resources/views/site/contacts.blade.php
@@ -16,26 +16,13 @@
       <div class="grid_6">
         <h2>Informações de contato</h2>
         <br>
-        {{-- <div class="map">
-          <figure class="img_inner">
-            <iframe src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Brooklyn,+New+York,+NY,+United+States&amp;aq=0&amp;sll=37.0625,-95.677068&amp;sspn=61.282355,146.513672&amp;ie=UTF8&amp;hq=&amp;hnear=Brooklyn,+Kings,+New+York&amp;ll=40.649974,-73.950005&amp;spn=0.01628,0.025663&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>
-          </figure>
-          <address>
-          <dl>
-            <dt>
-              <p>The Company Name Inc.<br>
-                8901 Marmora Road,<br>
-                Glasgow, D04 89GR.</p>
-            </dt>
-            <dd><span>Freephone:</span>+1 800 559 6580</dd>
-            <dd><span>Telephone:</span>+1 800 603 6035</dd>
-            <dd><span>FAX:</span>+1 800 889 9898</dd>
-          </dl>
-          </address>
-        </div> --}}
         <div>Faça uma doação para que possamos continuar com esse lindo projeto</div>
         <br />
-        <div><strong>Chave Pix</strong>: <span class="pix-code">dc25aa47-1dbf-4562-ac1f-195223e94457<span/></div>
+        <div><strong>Chave Pix</strong>: <span class="pix-code">dc25aa47-1dbf-4562-ac1f-195223e94457</span></div>
+        <div class="text-center" style="margin-top: 40px;">
+          <span>Ou escaneie o qr-code abaixo:</span>
+          <img src="./images/qrcode-pix.png" alt="">
+        </div>
       </div>
       <div class="grid_5 prefix_1">
         <h2 class="ic1">Contato</h2>
